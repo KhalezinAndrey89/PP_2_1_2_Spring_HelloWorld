a568301ca1abc77a119b52993e3bdcbf4b0a89f6
Andrey <KhalezinAndrey89@gmail.com>
2022-12-06 13:24:52 +0300

    Lesson 2.1.3. version 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat benCat1 = (Cat) applicationContext.getBean("This is a cat1");
+        Cat benCat2 = (Cat) applicationContext.getBean("This is a cat2");
+
+
+        System.out.println(bean1 == bean2);
+        System.out.println(benCat1 == benCat2);
+
     }
 }
